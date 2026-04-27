15be001cec1dc4887cb9fb892ae51da2e18250b7
HectorZ70 <hector.zornoza@enti.cat>
2026-04-27 11:21:16 +0200

    Steering Fixing

---
+++ ProyectoIII_01/Plugins/UINaviga56d41da4976eV4/UINaviga56d41da4976eV4/Intermediate/Build/Mac/UnrealEditor/Inc/UINavigation/UHT/UINavInputComponent.generated.h
@@ -0,0 +1,56 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "UINavInputComponent.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UINAVIGATION_UINavInputComponent_generated_h
+#error "UINavInputComponent.generated.h already included, missing '#pragma once' in UINavInputComponent.h"
+#endif
+#define UINAVIGATION_UINavInputComponent_generated_h
+
+#define FID_Build_U5M_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_UINavigation_Source_UINavigation_Public_UINavInputComponent_h_18_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUUINavInputComponent(); \
+	friend struct Z_Construct_UClass_UUINavInputComponent_Statics; \
+public: \
+	DECLARE_CLASS(UUINavInputComponent, UUINavComponent, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/UINavigation"), NO_API) \
+	DECLARE_SERIALIZER(UUINavInputComponent)
+
+
+#define FID_Build_U5M_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_UINavigation_Source_UINavigation_Public_UINavInputComponent_h_18_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UUINavInputComponent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UUINavInputComponent(UUINavInputComponent&&); \
+	UUINavInputComponent(const UUINavInputComponent&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UUINavInputComponent); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UUINavInputComponent); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UUINavInputComponent) \
+	NO_API virtual ~UUINavInputComponent();
+
+
+#define FID_Build_U5M_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_UINavigation_Source_UINavigation_Public_UINavInputComponent_h_15_PROLOG
+#define FID_Build_U5M_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_UINavigation_Source_UINavigation_Public_UINavInputComponent_h_18_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Build_U5M_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_UINavigation_Source_UINavigation_Public_UINavInputComponent_h_18_INCLASS_NO_PURE_DECLS \
+	FID_Build_U5M_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_UINavigation_Source_UINavigation_Public_UINavInputComponent_h_18_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UINAVIGATION_API UClass* StaticClass<class UUINavInputComponent>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Build_U5M_Mac_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_UINavigation_Source_UINavigation_Public_UINavInputComponent_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
